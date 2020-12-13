100369ccdd484e983e76cf575a38a0bc2ff2add9
Alex <kacloud@ncsu.edu>
2020-12-12 21:48:48 -0500

    add reward

--- code/data_generation.py
+++ code/data_generation.py
@@ -3,7 +3,7 @@ from scipy.special import expit
 
 np.set_printoptions(precision=3)
 
-# Feature values at time t describe the period time (t-1,t]
+# Feature values at time t describe the time period (t-1,t]
 feature_names = [
     "stress",
     "fatigue",
@@ -36,17 +36,22 @@ gluc_act_effect = -2
 gluc_min, gluc_max = (50, 250)
 
 stress_mean = 4.5
-stress_std = 3
+stress_std = 2
 stress_min, stress_max = (0, 15)
 
 class DiabetesTrial:
-    def __init__(self, n, t_total, initial_states=None):
+    def __init__(self, n, t_total, initial_states=None, compute_rewards=True):
         self.n = n
         self.t_total = t_total
         self.S = np.full((n, t_total+1, p), np.nan) # States (patient features)
         self.A = np.full((n, t_total, 2), np.nan) # Actions (insulin x messaging)
         self.T_dis = np.full(n, np.inf) # Per-patient disengagement times
-        self.R = np.full((n, t_total), np.nan) # Observed utilities
+        
+        # Save compute during burn-in by skipping calculation of utility.
+        if compute_rewards:
+            self.R = np.full((n, t_total), np.nan) # Observed utilities
+        else: 
+            self.R = None
         
         if initial_states is None:
             initial_states = [stress_mean, 0, 0, 0, 0, 0, 0, 0, 0, 0, gluc_mean, 0]
@@ -61,12 +66,14 @@ class DiabetesTrial:
             self.n, self.t_total, np.sum(self.engaged_inds), self.t
         )
     
-    def get_S(self, feature_name):
+    def get_S(self, feature_name, t=None):
         """ 
         Convenience function for getting the value of engaged patient features
         at the CURRENT time step.
         """
-        return self.S[self.engaged_inds, self.t, s_idx[feature_name]]
+        if t is None:
+            t = self.t
+        return self.S[self.engaged_inds, t, s_idx[feature_name]]
         
     def set_S(self, feature_name, feature_value):
         """ 
@@ -84,7 +91,7 @@ class DiabetesTrial:
         self.A[self.engaged_inds, self.t, a_idx[action_name]] = action_value
     
     def _apply_dropout(self):
-        dropout_probs = 0.2*expit(self.get_S("stress")-8)+ 0.8 * 0.02
+        dropout_probs = 0.2*expit(self.get_S("stress")-8) + 0.8 * 0.02
         dropout_inds = np.random.binomial(1, dropout_probs)
         
         engaged_inds_before = self.engaged_inds.copy()
@@ -109,9 +116,13 @@ class DiabetesTrial:
             0.1*stress_mean + 0.9*self.get_S("stress") 
             - 2 * message_ind * (1-prev_fatigue)
             + np.random.normal(0, stress_std, size=n_engaged)
-        ).clip(stress_min, stress_max).round(0)
+        ).clip(stress_min, stress_max).round(1)
           
-        fatigue = (0.95*prev_fatigue**2 + 0.4*message_ind).clip(0,1).round(2)
+        fatigue = (
+            0.95*prev_fatigue**2 
+            + 0.4*message_ind
+            + np.random.normal(0,0.1, size=n_engaged)
+        ).clip(0,1).round(2)
         
         exercise = (
             (np.random.binomial(1, 0.2, size=n_engaged) 
@@ -120,7 +131,7 @@ class DiabetesTrial:
              * np.random.normal(31, 5, size=n_engaged)).clip(min=0)
         )
         
-        expit_stress = expit( (self.get_S("stress") - 7)/4 )
+        expit_stress = expit((self.get_S("stress") - 7)/4) 
                 
         eat_ind = np.random.binomial(1, 0.1 + 0.3*expit_stress, size=n_engaged)
         unhealthy_snack_ind = (
@@ -157,31 +168,42 @@ class DiabetesTrial:
         self.set_S("insulin-1", prev_insulin)
         
     def _compute_rewards(self):
-        pass
+        """ Composite reward for exercise and avoiding unhealthy eating. """
+        self.R[self.engaged_inds, self.t] = (
+            self.get_S("exercise", self.t+1) / 800 
+            - self.get_S("unhealthy_ind", self.t+1)
+        )
         
     def step_forward_in_time(self, policy, apply_dropout):
         if self.t >= self.t_total:
             raise RuntimeError(f"The trial is over; t={self.t}.")
         
         if apply_dropout:
-            self._apply_dropout()    
-            
+            self._apply_dropout()        
         self._compute_actions(policy)            
         self._apply_state_transition()
-        self._compute_rewards()
+        
+        if self.R is not None:
+            self._compute_rewards()
         self.t += 1
+    
+    def get_returns(self):
+        """ Return per-patient total rewards. """
+        return np.nansum(self.R, axis=1)
+        
 
-def get_burned_in_states(n, t_burn, mu_burn):
-    trial_burn = DiabetesTrial(n=n, t_total=t_burn)
+def get_burned_in_states(n, mu_burn, t_burn=50):
+    trial_burn = DiabetesTrial(n=n, t_total=t_burn, compute_rewards=False)
     for t in range(t_burn):
         trial_burn.step_forward_in_time(mu_burn, apply_dropout=False)
     initial_states = trial_burn.S[:,-1,:]
     return initial_states
-    
-t_max = 24   
-n = 100
-mu = lambda x: np.full_like(x,fill_value=0.3)
 
-trial = DiabetesTrial(n, t_max, initial_states=get_burned_in_states(n, 50, mu))
-for t in range(t_max):
-    trial.step_forward_in_time(mu, apply_dropout=False)
+if __name__ == "__main__":
+    t_max = 24   
+    n = 100
+    mu = lambda x: np.full_like(x,fill_value=0.3)
+    
+    trial = DiabetesTrial(n, t_max, initial_states=get_burned_in_states(n, mu))
+    for t in range(t_max):
+        trial.step_forward_in_time(mu, apply_dropout=False)
--- code/data_visualization.py
+++ code/data_visualization.py
@@ -4,20 +4,28 @@ import matplotlib.pyplot as plt
 import data_generation as dg
 
 def plot_feature_over_time(feature, trial, num_to_plot=10):
-    stress_over_time = trial.S[range(num_to_plot),:,dg.s_idx[feature]]
+    feature_over_time = trial.S[range(num_to_plot),:,dg.s_idx[feature]]
     fig, ax = plt.subplots()
-    ax.plot(stress_over_time.T)
+    ax.plot(feature_over_time.T)
     ax.set_title(f"Patient {feature} over time")
     ax.set_xlabel("time")
     ax.set_ylabel(feature)
     plt.show()
-    
+
+def plot_feature_dist(feature, trial):
+    fig, ax = plt.subplots()
+    all_feature = trial.S[:,:,dg.s_idx[feature]]
+    flattened = all_feature[~np.isnan(all_feature)].flatten()
+    ax.hist(flattened, bins=30, density=True)
+    ax.set_xlabel(feature)
+    ax.set_title(f"Distribution of {feature} across patients, time")
+    plt.show()
 
 t_max = 48  
 n = 100
 mu = lambda x: np.full_like(x,fill_value=0.3)
 
-trial = dg.DiabetesTrial(n, t_max, initial_states=dg.get_burned_in_states(n, 50, mu))
+trial = dg.DiabetesTrial(n, t_max, initial_states=dg.get_burned_in_states(n, mu))
 for t in range(t_max):
     trial.step_forward_in_time(mu, apply_dropout=True)
 
@@ -30,3 +38,6 @@ plot_feature_over_time("stress", trial)
 plot_feature_over_time("fatigue", trial)
 plot_feature_over_time("glucose", trial)
 plot_feature_over_time("insulin-1", trial)
+
+plot_feature_dist("exercise", trial)
+plot_feature_dist("unhealthy_ind", trial)
