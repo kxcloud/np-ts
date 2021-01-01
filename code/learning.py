6f7ce3eac102ace98d751dcb48ccc79166c586b5
Alex <kacloud@ncsu.edu>
2020-12-31 19:57:32 -0500

    refactor value estimation
    todo: remove old version and add policy optimization

--- code/learning.py
+++ code/learning.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.special import softmax
 from scipy.optimize import minimize 
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
 
 import data_generation as dg
 
@@ -17,8 +19,11 @@ class Policy():
         
         self.param_size = len(self.beta)
         
-    def act(self, states):
-        action_probs = softmax(self.state_encoding(states) @ self.beta, axis=1)
+    def act(self, states, apply_encoding=True):
+        if apply_encoding:
+            action_probs = softmax(self.state_encoding(states) @ self.beta, axis=1)
+        else:
+            action_probs = softmax(states @ self.beta, axis=1)
         return action_probs
     
     def prob_of(self, state, action):
@@ -54,6 +59,60 @@ for t in range(t_max):
 
 policy = Policy(beta_0.copy())    
 
+def get_optimization_terms(trial, state_encoding, discount):
+    """ 
+    Get feature matrix, reward vector, and other components for quick value 
+    estimation and policy optimization. Includes weighting by behavior policy 
+    but NOT by target.
+    """
+    total_num_actions_taken = 0
+    for i in range(trial.n):
+        total_num_actions_taken += trial.num_treatments_applied(i)
+    
+    feature_matrix = np.zeros((total_num_actions_taken, policy.param_size), dtype=np.float64)
+    rewards = np.zeros((total_num_actions_taken), dtype=np.float64)
+    encoded_states = np.zeros((total_num_actions_taken, policy.param_size))
+    actions = np.zeros((total_num_actions_taken), dtype=int)
+    term_idx = 0
+    for i in range(trial.n):
+        last_t = trial.num_treatments_applied(i)
+        phi_s_next = None
+        for t in range(last_t):
+            phi_s = phi_s_next if t > 0 else state_encoding(trial.S[i,t,:])
+            phi_s_next = state_encoding(trial.S[i,t+1,:]) if t < trial.T_dis[i] else 0
+            
+            feature_matrix[term_idx, :] = (
+                discount * phi_s_next - phi_s
+            ) / trial.A_prob[i, t]
+            rewards[term_idx] = trial.R[i,t] / trial.A_prob[i, t]
+            encoded_states[term_idx] = phi_s
+            actions[term_idx] = int(trial.A[i,t])
+            term_idx += 1
+    
+    return feature_matrix, rewards, encoded_states, actions
+
+def get_policy_probs(policy, encoded_states, actions):
+    all_action_probs = policy.act(encoded_states, apply_encoding=False)
+    total_num_actions_taken = len(actions)
+    selected_action_probs = np.zeros(total_num_actions_taken)
+    for idx in range(total_num_actions_taken):
+        selected_action_probs[idx] = all_action_probs[idx,actions[idx]]
+    return selected_action_probs
+
+def fit_value_fn(trial, policy, discount=0.99, ridge_penalty=0):
+    feature_matrix, rewards, encoded_states, actions = get_optimization_terms(
+        trial, policy.state_encoding, discount
+    )
+    
+    policy_probs = get_policy_probs(policy, encoded_states, actions)
+    
+    wt_feature_matrix = policy_probs[:,None] * feature_matrix
+    wt_rewards = policy_probs * rewards
+    
+    reg = Ridge(fit_intercept=False, alpha=ridge_penalty).fit(-wt_feature_matrix, wt_rewards)
+    theta_hat2 = reg.coef_
+    return theta_hat2
+    
 def get_value_fn_optimizer(trial, policy, discount=0.99):
     """
     Precompute terms used for calculating the mean square Bellman Error, then
@@ -89,15 +148,16 @@ def get_value_fn_optimizer(trial, policy, discount=0.99):
     
     return mean_square_bellman_error, all_phi_terms, all_rewards
 
+theta_hat3 = fit_value_fn(trial, policy)
+
 msbe, all_phi_terms, all_rewards = get_value_fn_optimizer(trial, policy)
 bfgs_obj = minimize(msbe, np.zeros(len(dg.feature_names)))
 theta_hat1 = bfgs_obj.x
 
-from sklearn.linear_model import LinearRegression
 reg = LinearRegression(fit_intercept=False).fit(-all_phi_terms, all_rewards)
 
 theta_hat2 = reg.coef_
 
-print(theta_hat1)
-print(theta_hat2)
+print(theta_hat1.round(2))
+print(theta_hat3.round(2))
 print(f"Abs total diff: {np.abs(theta_hat1-theta_hat2).sum()}")
